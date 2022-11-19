4cdd4061969a9f1fa55580bb4edb9d01af8b005e
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-19 16:15:26 +0100

    ✅ Add pass test

--- src/db.conect.test.ts
+++ src/db.conect.test.ts
@@ -7,4 +7,11 @@ describe('Given dbConnect', () => {
         expect(typeof result).toBe(typeof mongoose);
         mongoose.disconnect();
     });
+
+    test('When we instantiate it should make a connection', async () => {
+        process.env.NODE_ENV = 'WeekendChallenge';
+        const result = await dbConnect();
+        expect(typeof result).toBe(typeof mongoose);
+        mongoose.disconnect();
+    });
 });
