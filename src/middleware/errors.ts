a0cfcf5a5dbe279ce763b8e724dc219356ab8b28
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-21 20:12:48 +0100

    ⚡️ Adding middleware

---
+++ src/middleware/errors.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { CustomError } from '../interface/error';
+
+export const errorManager = () => {
+    (error: CustomError, _req: Request, resp: Response, next: NextFunction) => {
+        next;
+        console.log(
+            error.name,
+            error.statusCode,
+            error.statusMessage,
+            error.message
+        );
+        let status = error.statusCode || 500;
+        if (error.name === 'ValidationError') {
+            status = 406;
+        }
+        const result = {
+            status: status,
+            type: error.name,
+            error: error.message,
+        };
+        resp.status(status).json(result).end();
+    };
+};
