96ed805b5893d20d14c53c020d283b46bb1856c0
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-24 10:08:00 +0100

    🤡 Put beforeEach in the right place

--- src/e2e/users.spec.ts
+++ src/e2e/users.spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { app } from '../app';
 import { dbConnect } from '../db.conect';
 import { User } from '../entities/users';
-import { createToken, TokenPayload } from '../services/auth';
+import { TokenPayload } from '../services/auth';
 
 const setCollection = async () => {
     const mockData = [
@@ -28,23 +28,23 @@ const setCollection = async () => {
 };
 
 describe('Given an app with "/users/" route', () => {
-    let ids: Array<string>;
-
-    beforeEach(async () => {
-        await dbConnect();
-        ids = await setCollection();
-        const payload: TokenPayload = {
-            id: ids[0],
-            name: 'Pepe',
-            role: 'Admin',
-        };
-    });
-
-    afterEach(async () => {
-        await mongoose.disconnect();
-    });
-
     describe('When we connect with MongoDB', () => {
+        let ids: Array<string>;
+
+        beforeEach(async () => {
+            await dbConnect();
+            ids = await setCollection();
+            const payload: TokenPayload = {
+                id: ids[0],
+                name: 'Pepe',
+                role: 'Admin',
+            };
+        });
+
+        afterEach(async () => {
+            await mongoose.disconnect();
+        });
+
         test('Then the post() to url /register should send an status 201', async () => {
             const response = await request(app).post('/users/register').send({
                 name: 'Amelia',
