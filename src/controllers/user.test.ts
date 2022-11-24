5367edb5697cf60143ed9cf33a3cd3a82a245936
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-24 13:56:20 +0100

    ✅ Add pass tests

--- src/controllers/user.test.ts
+++ src/controllers/user.test.ts
@@ -4,18 +4,57 @@ import { RobotRepository } from '../repository/robots';
 import { UsersRepository } from '../repository/users';
 import { UsersController } from './users';
 
+jest.mock('../services/auth');
+
+const mockData = [{ name: 'Pepe' }, { name: 'Ernesto' }];
+
+describe('Given the users controller,', () => {
+    let repository: RobotRepository;
+    let userRepo: UsersRepository;
+    let userController: UsersController;
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let next: NextFunction;
+
+    beforeEach(() => {
+        repository = RobotRepository.getInstance();
+        userRepo = UsersRepository.getInstance();
+
+        userRepo.get = jest.fn().mockResolvedValue(mockData[0]);
+        userRepo.post = jest.fn().mockResolvedValue(mockData[0]);
+        userController = new UsersController(userRepo, repository);
+
+        req = {};
+        res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn();
+        next = jest.fn();
+    });
+
+    describe('When we instantiate register(),', () => {
+        test('It should create a new user', async () => {
+            req.body = { mockData };
+            await userController.register(
+                req as Request,
+                res as Response,
+                next
+            );
+            expect(res.json).toHaveBeenCalledWith({ user: mockData[0] });
+        });
+    });
+});
+
 describe('Given the users controller, but everything goes wrong', () => {
     const error: CustomError = new HTTPError(
         404,
         'Not found id',
         'message of error'
     );
-
-    UsersRepository.prototype.get = jest.fn().mockRejectedValue('User');
-    UsersRepository.prototype.post = jest.fn().mockRejectedValue(['User']);
-
     const repository = RobotRepository.getInstance();
     const userRepo = UsersRepository.getInstance();
+
+    userRepo.get = jest.fn().mockRejectedValue('User');
+    userRepo.post = jest.fn().mockRejectedValue(['User']);
     const userController = new UsersController(userRepo, repository);
 
     const req: Partial<Request> = {};
